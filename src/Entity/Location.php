a464cd29cf5d352d3d18eed33b6174f3bba89d21
wendy1976 <caroline.ferru@free.fr>
2024-01-15 18:23:37 +0100

    add entity Location

---
+++ src/Entity/Location.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+#[Broadcast]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $category = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $coordinates = [];
+
+    #[ORM\Column(type: Types::OBJECT)]
+    private ?object $icon = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $popupContent = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getCoordinates(): array
+    {
+        return $this->coordinates;
+    }
+
+    public function setCoordinates(array $coordinates): static
+    {
+        $this->coordinates = $coordinates;
+
+        return $this;
+    }
+
+    public function getIcon(): ?object
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(object $icon): static
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPopupContent(): ?string
+    {
+        return $this->popupContent;
+    }
+
+    public function setPopupContent(string $popupContent): static
+    {
+        $this->popupContent = $popupContent;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
