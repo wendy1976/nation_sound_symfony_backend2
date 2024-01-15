1e6cdb003b119468c2478c1d1e3d19126f94ac87
wendy1976 <caroline.ferru@free.fr>
2024-01-15 10:00:09 +0100

    feat dateToFrench in DateConcert

--- src/Entity/DateConcert.php
+++ src/Entity/DateConcert.php
@@ -56,8 +56,17 @@ class DateConcert
         return $this;
     }
 
+    private function dateToFrench(\DateTimeInterface $date, $format) {
+        $english_days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        $french_days = array('lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche');
+        $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        $french_months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+        $date_string = $date->format('Y-m-d H:i:s');
+        return str_replace($english_months, $french_months, str_replace($english_days, $french_days, date($format, strtotime($date_string))));
+    }
+
     public function __toString(): string
     {
-        return $this->date->format('d M Y \à H:i');
+        return $this->dateToFrench($this->date_heure, 'd F Y \à H:i');
     }
 }
